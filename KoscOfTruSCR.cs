2e63ec388b003462776f0f467f3afb2f0fcd0b09
Maciej Dobrowolski <45076427+Mahox99@users.noreply.github.com>
2021-03-11 23:12:43 +0100

    Add files via upload

---
+++ KoscOfTruSCR.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class KoscOfTruSCR : MonoBehaviour
+{
+    public float Distance;
+    public GameObject ActionText;
+    public GameObject tlo;
+    public GameObject Cube;
+    void Update()
+    {
+        Distance = PLayerCasting.DistanceFromTarget;
+    }
+
+    void OnMouseOver()
+    {
+        if (Distance <= 3)
+        {
+            ActionText.SetActive(true);
+            tlo.SetActive(true); 
+        }
+        else
+        {
+            ActionText.SetActive(false); 
+            tlo.SetActive(false);
+        }
+    }
+    void OnMouseExit()
+    {
+        ActionText.SetActive(false);
+        tlo.SetActive(false);
+    }
+}
