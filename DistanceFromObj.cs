d57b6f38571bee7ba525ca70588344b941a25b1b
Maciej Dobrowolski <45076427+Mahox99@users.noreply.github.com>
2021-04-15 18:31:51 +0200

    Add files via upload
    AI for enemies.
    When player is too close, Enemy object will follow player.

---
+++ DistanceFromObj.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceFromObj : MonoBehaviour
+{
+    public GameObject Obj1;
+    public GameObject Obj2;
+    public static float Dis;
+ 
+    void Update()
+    {
+        Dis = Vector3.Distance(Obj1.transform.position, Obj2.transform.position);
+    }
+}
---
+++ EnemyFollowing.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowing : MonoBehaviour
+{
+    public float Start_Search_Distanse;
+    public float Stop_Search_Distanse;
+    public float Distance;
+
+    public GameObject[] waypoints;
+    public float WPradius;
+    int current = 0;
+
+    Transform tr_player;
+    float f_rotateSpeed = 3.0f, f_moveSpeed = 3.0f;
+    bool search = false;
+
+    void Start()
+    {
+        tr_player = GameObject.FindGameObjectWithTag("Player").transform;    
+    }
+
+    void Update()
+    {
+        Distance = DistanceFromObj.Dis;
+        
+        if (Distance <= Start_Search_Distanse)
+        {
+            search = true;
+        }
+        else if (Distance >= Stop_Search_Distanse)
+        {
+            search = false;
+        }
+
+        if (search == true)
+        {
+
+            //Patrzenie na gracza       //looking for the player
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(
+            tr_player.position - transform.position), f_rotateSpeed * Time.deltaTime);
+
+            //ruch w kierunku gracza    //move to player
+            transform.position += transform.forward * f_moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+            {
+                current++;
+                if (current >= waypoints.Length)
+                {
+                    current = 0;
+                }
+            }
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * f_moveSpeed);
+        }
+    }
+}
